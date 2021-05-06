667f0721fca8e09ac3adc0d84ca6608d03647dc0
jkunciene <j.kunciene@gmail.com>
2021-05-06 10:31:37 +0300

    initial commit

---
+++ src/components/Cart.js
@@ -0,0 +1,14 @@
+import React, { useReducer, useContext, createContext } from 'react';
+
+const CartStateContext = createContext();
+const CartDispatchContext = createContext();
+
+const Cart = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Cart
